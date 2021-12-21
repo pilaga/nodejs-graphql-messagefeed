57a8f0510c784fe797b32c12c9fba1ae0ecc92e7
Pierre <pierre.lagadec@gmail.com>
2021-12-22 10:23:24 +1100

    initial set up

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+
+const feedRoutes = require('./routes/feed');
+
+const app = express();
+
+app.use(bodyparser.json());
+
+//add headers on any incoming response to allow cross origin requests
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Conrol-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
+
+app.use('/feed', feedRoutes);
+
+app.listen(8080);
