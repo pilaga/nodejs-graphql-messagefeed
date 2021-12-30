21a8840f5bebf1a75bfb2dd3e5551a6747947883
Pierre <pierre.lagadec@gmail.com>
2021-12-30 11:55:27 +1100

    serving static images & error handling

--- rest-backend/app.js
+++ rest-backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const feedRoutes = require('./routes/feed');
 
@@ -9,6 +10,7 @@ const MONGODB_URI = 'mongodb+srv://admin:password_02@cluster0.lrvxm.mongodb.net/
 const app = express();
 
 app.use(bodyparser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 //add headers on any incoming response to allow cross origin requests
 app.use((req, res, next) => {
@@ -19,6 +21,13 @@ app.use((req, res, next) => {
 })
 
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode;
+    const message = error.message;
+    res.status(status).json({ message: message });
+});
 
 mongoose.connect(MONGODB_URI)
 .then(result => {
