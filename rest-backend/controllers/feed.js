a20c2a4490a6b6f67a99f5c111c1577aab8c85cb
Pierre <pierre.lagadec@gmail.com>
2022-01-02 10:27:12 +1100

    updating posts

--- rest-backend/controllers/feed.js
+++ rest-backend/controllers/feed.js
@@ -1,4 +1,6 @@
 const { validationResult } = require('express-validator');
+const fs = require('fs');
+const path = require('path');
 
 const Post = require('../models/post');
 
@@ -79,3 +81,63 @@ exports.createPost = (req, res, next) => {
         next(err);
     });
 };
+
+exports.updatePost = (req, res, next) => {
+    const postId = req.params.postId;
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        const error = new Error('Validation failed, entered data is incorrect');
+        error.statusCode = 422;
+        throw error;
+    }
+    const title = req.body.title;
+    const content = req.body.content;
+    let imageUrl = req.body.image;
+    if(req.file) {
+        imageUrl = req.file.path;
+    }
+    if(!imageUrl) {
+        const error = new Error('No image file picked');
+        error.statusCode = 422;
+        throw error;
+    }
+    //if we reached here, data is valid - update
+    Post.findById(postId)
+    .then(post => {
+        if(!post) {
+            const error = new Error('Could not find post to update');
+            error.statusCode = 404;
+            throw error;
+        }
+        //if image changes, delete old image
+        if(imageUrl !== post.imageUrl) {
+            clearImage(post.imageUrl);
+        }
+        //update post data
+        post.title = title;
+        post.imageUrl = imageUrl;
+        post.content = content;
+        return post.save();
+    })
+    .then(result => {
+        res.status(200).json({
+            message: 'Post updated successfully',
+            post: result
+        })
+    })
+    .catch(err => {
+        if(!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    });
+};
+
+//delete image helper function
+const clearImage = filepath => {
+    filepath = path.join(__dirname, '..', filepath);
+    fs.unlink(filepath, err => {
+        if(err)
+        console.log(err);
+    })
+}
--- rest-frontend/src/pages/Feed/Feed.js
+++ rest-frontend/src/pages/Feed/Feed.js
@@ -59,7 +59,12 @@ class Feed extends Component {
       })
       .then(resData => {
         this.setState({
-          posts: resData.posts,
+          posts: resData.posts.map(post => {
+            return {
+              ...post,
+              imagePath: post.imageUrl
+            }
+          }),
           totalPosts: resData.totalItems,
           postsLoading: false
         });
@@ -105,14 +110,17 @@ class Feed extends Component {
     this.setState({
       editLoading: true
     });
+
     const formData = new FormData();
     formData.append('title', postData.title);
     formData.append('content', postData.content);
     formData.append('image', postData.image);
+    
     let url = 'http://localhost:8080/feed/post';
     let method = 'POST';
     if (this.state.editPost) {
-      url = 'URL';
+      url = 'http://localhost:8080/feed/post/' + this.state.editPost._id;
+      method = 'PUT';
     }
 
     fetch(url, {
