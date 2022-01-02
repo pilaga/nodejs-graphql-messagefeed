a4c5bf0801383e11384e46cd8484946e1b043190
Pierre <pierre.lagadec@gmail.com>
2022-01-02 14:08:05 +1100

    login in and creating JSON Web Token (JWT)

--- rest-backend/controllers/auth.js
+++ rest-backend/controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
     //check for input errors
@@ -57,7 +58,20 @@ exports.login = (req, res, next) => {
             error.statusCode = 401;
             throw error;
         }
-        
+        const token = jwt.sign(
+            {
+                email: loadedUser.email,
+                userId: loadedUser._id.toString()
+            }, 
+            'my-secret-key',
+            {
+                expiresIn: '1h'
+            }
+        );
+        res.status(200).json({
+            token: token,
+            userId: loadedUser._id.toString()
+        });
     })
     .catch(err => {
         if(!err.statusCode) {
