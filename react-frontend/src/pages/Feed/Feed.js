435278a93c735b844396054bb0fb54c96567d780
Pierre <pierre.lagadec@gmail.com>
2022-01-03 13:34:59 +1100

    establishing a socket connection from the client

--- react-frontend/src/pages/Feed/Feed.js
+++ react-frontend/src/pages/Feed/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import openSocket from 'socket.io-client';
 
 import Post from '../../components/Feed/Post/Post';
 import Button from '../../components/Button/Button';
@@ -39,6 +40,9 @@ class Feed extends Component {
       .catch(this.catchError);
 
     this.loadPosts();
+
+    //set-up socket client
+    openSocket('http://localhost:8080');
   }
 
   loadPosts = direction => {
