6a1940ba86c7e5c2e67ab9d2d08d45f49a8eb08a
Pierre <pierre.lagadec@gmail.com>
2022-01-06 11:48:52 +1100

    graphql - adding input validation for new User

--- rest-backend/graphql/resolvers.js
+++ rest-backend/graphql/resolvers.js
@@ -1,10 +1,22 @@
-const User = require('../models/user');
 const bcrypt = require('bcryptjs');
+const validator = require('validator');
 
+const User = require('../models/user');
 
 module.exports = {
     createUser: async function({ userInput }, req) {
         //if using then/catch instead of async/await, need to return the User.findOne() result
+        const validationErrors = [];
+        if(!validator.isEmail(userInput.email)) {
+            validationErrors.push({ message: 'Email is invalid' });
+        }
+        if(validator.isEmpty(userInput.password) || !validator.isLength(userInput.password, { min: 5 })) {
+            validationErrors.push({ message: 'Password is not long enough' });
+        }
+        if(validationErrors.length > 0) {
+            const error = new Error('Invalid input');
+            throw error;
+        }
         const existingUser = await User.findOne({ email: userInput.email });
         if(existingUser) {
             const error = new Error('User exists already');
